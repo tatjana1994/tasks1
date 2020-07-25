71a575c5acde31896f2cd6195a7a68363060e682
Tatjana Devrnja <devrnjat94@gmail.com>
2020-07-25 17:04:26 +0200

    izmene

--- UserRepo.js
+++ UserRepo.js
@@ -1,13 +1,13 @@
 class UserRepo {
     constructor() {
     this.users = [];
-}
+    }
 
 create(obj){  
 
     this.users.push(obj)
 
-}
+    }
 
 
 read(ID){
@@ -15,13 +15,13 @@ read(ID){
     let user = this.users.find(item => item.id == ID);
     return user;
 
-}   
+    }   
 
 update(ID, prop, value){
     user = this.read(ID);
     user[prop] = value;
     
-}
+    }
 
 delete(ID) {
     user = this.read(ID);
@@ -32,27 +32,5 @@ delete(ID) {
 
 }
 
-let user =  {
-    id : 1,
-    firstName : "john",
-    surname :  "smith"
 
-}
-
-let user1 =  {
-    id : 2,
-    firstName : "john",
-    surname :  "smith"
-
-}
-
-
-let userRepository = new UserRepo();
-
-// userRepository.create(user);
-// userRepository.create(user1);
-
-
-// console.log(userRepository.delete(1));
-// console.log(userRepository);
 
